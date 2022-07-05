97352d96f112035ea03172073c6326959bfc41a6
Huang Feilong <hfl@huangfeilong.com>
2022-07-05 17:09:20 +0800

    fix admin

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "popper"
-import "bootstrap"
-import "scripts"
-import "datatables-simple-demo"
-import "chart-area-demo"
-import "chart-bar-demo"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "popper";
+import "bootstrap";
+import "chart-area-demo";
+import "chart-bar-demo";
+import "scripts";
+import "datatables-simple-demo";
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,9 +5,9 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
-pin "scripts", to: 'admin/scripts.js', preload: true
-pin "datatables-simple-demo", to: 'admin/datatables-simple-demo.js', preload: true
-pin "chart-area-demo", to: 'admin/chart-area-demo.js', preload: true
-pin "chart-bar-demo", to: 'admin/chart-bar-demo.js', preload: true
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "chart-area-demo", to: "admin/chart-area-demo.js", preload: true
+pin "chart-bar-demo", to: "admin/chart-bar-demo.js", preload: true
+pin "scripts", to: "admin/scripts.js", preload: true
+pin "datatables-simple-demo", to: "admin/datatables-simple-demo.js", preload: true
